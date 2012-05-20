8ec81c3dca0f13c3997f39c45b497703504e8abb
thecom <oluensdorf@gmail.com>
2012-05-20 20:27:00 +0200

    Added resize function to renderer and minor cleanups.

--- pano.js
+++ pano.js
@@ -89,7 +89,8 @@ function resizeCanvas() {
   pano_canvas.height = newHeight;
 
   target = null;
-  draw();
+  renderer.resize();
+  renderer.draw();
 }
 
 function imageLoaded(){
@@ -109,7 +110,7 @@ function imageLoaded(){
 
   //get pixels
   source = buffer_ctx.getImageData(0, 0, tex_resolution, tex_resolution);
-  renderer.setImage(source, img.height * scale);
+  renderer.set_image(source, img.height * scale);
 
   resizeCanvas();
 }
@@ -126,18 +127,18 @@ function mouseMove(e){
     cam_pitch += 0.25*(e.clientY-mouseDownPosLastY);
     mouseDownPosLastX = e.clientX;
     mouseDownPosLastY = e.clientY;
-    draw();
+    renderer.draw();
   }
 }
 
 function mouseUp(e){
   mouseIsDown = false;
-  draw();
+  renderer.draw();
 }
 
 function mouseScroll(e){
   cam_fov+=e.wheelDelta/120;
-  draw();
+  renderer.draw();
 }
 
 function keyDown(e) {
@@ -145,17 +146,17 @@ function keyDown(e) {
     // i = info
     case 73:
       displayInfo = !displayInfo;
-      draw();
+      renderer.draw();
       break;
     // m = menu
     case 77:
       displayMenu = !displayMenu;
-      draw();
+      renderer.draw();
       break;
     // ? = help
     case 191:
       displayHelp = !displayHelp;
-      draw();
+      renderer.draw();
       break;
     default:
       console.log("key: ", e.keyCode);
@@ -192,199 +193,6 @@ function drawLine(pixels, scanwidth, xofs, x0, y0, x1, y1) {
   }
 }
 
-draw_count = 0;
-function drawQuad(src, sscanwidth, sofs, tgt, tscanwidth, tofs,
-                  sxl0, syl0, sxl1, syl1, sxr0, syr0, sxr1, syr1,
-                  tx0, ty0, dtx, dty) {
-  var dxl = sxl1 - sxl0;
-  var dyl = syl1 - syl0;
-
-  var dxr = sxr1 - sxr0;
-  var dyr = syr1 - syr0;
-
-  var xl = sxl0, yl = syl0, xr = sxr0, yr = syr0;
-
-  var ty = Math.floor(ty0);
-  for(var row = 0; row < dty; row++) {
-    //if(ty > Math.floor(ty0) && ty < tyend - 1) continue;
-    // Draw a line from xl to xr
-    var ixl = Math.floor(xl), iyl = Math.floor(yl),
-    ixr = Math.floor(xr), iyr = Math.floor(yr);
-    var dx = Math.abs(ixr - ixl);
-    var dy = -Math.abs(iyr - iyl);
-    var dist = Math.sqrt(dx*dx + dy*dy);
-    var sx = (ixl < ixr) ? 1 : -1;
-    var sy = (iyl < iyr) ? 1 : -1;
-    var err1 = dx + dy;
-    var err2;
-
-    var steps = (dx > -dy) ? dx : -dy;
-    var tgt_step = dtx / steps;
-    var tgt_frac = 0;
-    var tgt_bla = 0;
-
-    var x = ixl;
-    var y = iyl;
-
-    var tx = Math.floor(tx0);
-    var txend = Math.floor(tx0 + dtx);
-    var tgt_offset = 4*(ty * tscanwidth + tx + tofs);
-    while(true) {
-      tgt_frac += tgt_step;
-      if(tgt_bla < tgt_frac)
-        var src_offset=4*(y * sscanwidth +
-                          (((x + sofs) % sscanwidth) + sscanwidth) % sscanwidth);
-
-      while(tx <= txend) {
-        if (tgt_bla >= tgt_frac) break;
-        tgt_bla++;
-        // Draw pixel.
-        tgt[tgt_offset]     = src[src_offset];
-        tgt[tgt_offset+1]   = src[src_offset+1];
-        tgt[tgt_offset+2]   = src[src_offset+2];
-        draw_count++;
-        tx++;
-        tgt_offset += 4;
-      }
-
-      if (x == ixr && y == iyr)
-        break;
-
-      err2 = 2*err1;
-      if(err2 > dy) {
-        err1 += dy;
-        x += sx;
-      }
-      if(err2 < dx) {
-        err1 += dx;
-        y += sy;
-      }
-    }
-
-    xl = sxl0 + dxl * row / dty;
-    yl = syl0 + dyl * row / dty;
-    xr = sxr0 + dxr * row / dty;
-    yr = syr0 + dyr * row / dty;
-    ty++;
-  }
-}
-
-function renderPanorama(canvas){
-  if(canvas != null && source != null){
-    var ctx         = canvas.getContext("2d");
-    var src_width   = img.width;
-    var src_height  = img.height;
-    var dest_width  = canvas.width;
-    var dest_height = canvas.height;
-
-    //ctx.drawImage(img, 0, 0, img.width, img.height, dest_width, 0,
-    //        dest_width, dest_height);
-
-    //calculate camera plane
-    var theta_fac = src_height/Math.PI;
-    var phi_fac = src_width*0.5/Math.PI
-    var ratioUp = 2.0*Math.tan(cam_fov*DEG2RAD/2.0);
-    var ratioRight = ratioUp*(16/9);
-    var camDirX = Math.sin(cam_pitch*DEG2RAD)*Math.sin(cam_heading*DEG2RAD);
-    var camDirY = Math.cos(cam_pitch*DEG2RAD);
-    var camDirZ = Math.sin(cam_pitch*DEG2RAD)*Math.cos(cam_heading*DEG2RAD);
-    var camUpX = ratioUp*Math.sin((cam_pitch-90.0)*DEG2RAD)*Math.sin(cam_heading*DEG2RAD);
-    var camUpY = ratioUp*Math.cos((cam_pitch-90.0)*DEG2RAD);
-    var camUpZ = ratioUp*Math.sin((cam_pitch-90.0)*DEG2RAD)*Math.cos(cam_heading*DEG2RAD);
-    var camRightX = ratioRight*Math.sin((cam_heading-90.0)*DEG2RAD);
-    var camRightY = 0.0;
-    var camRightZ = ratioRight*Math.cos((cam_heading-90.0)*DEG2RAD);
-    var camPlaneOriginX = camDirX + 0.5*camUpX - 0.5*camRightX;
-    var camPlaneOriginY = camDirY + 0.5*camUpY - 0.5*camRightY;
-    var camPlaneOriginZ = camDirZ + 0.5*camUpZ - 0.5*camRightZ;
-
-    var fx = 0.5, fy = 0.5;
-    var rayX = camPlaneOriginX + fx*camRightX - fy*camUpX;
-    var rayZ = camPlaneOriginZ + fx*camRightZ - fy*camUpZ;
-    var x_shift=(Math.atan2(rayZ,rayX) + Math.PI) * phi_fac;
-
-    var camDirX = 0.0;
-    var camDirZ = Math.sin(cam_pitch*DEG2RAD);
-    var camUpX = 0.0;
-    var camUpZ = ratioUp*Math.sin((cam_pitch-90.0)*DEG2RAD);
-    var camRightX = -ratioRight;
-    var camRightZ = 0.0;
-    var camPlaneOriginX = camDirX + 0.5*camUpX - 0.5*camRightX;
-    var camPlaneOriginY = camDirY + 0.5*camUpY - 0.5*camRightY;
-    var camPlaneOriginZ = camDirZ + 0.5*camUpZ - 0.5*camRightZ;
-
-    //render image
-    var i,j;
-    var frac_fx = 1/dest_width;
-    var frac_fy = 1/dest_height;
-    var fx = 0;
-    var fy = 0;
-    var min_y = 0;
-    var max_y = 0;
-    var res = 8;
-    var x_res = (dest_width - 1) / res, y_res = (dest_height - 1) / res;
-    var line_x1 = new Array(res+1);
-    var line_y1 = new Array(res+1);
-    var line_x2 = new Array(res+1);
-    var line_y2 = new Array(res+1);
-    draw_count = 0;
-    for(i=0; i<res+1; i++){
-      var fy = i/res;
-      var line_i = 0;
-
-      for(j=0; j<res+1; j++){
-        var fx = j/res;
-
-        /* LOOKUP Tabellen für acos und atan2? */
-        /* Bringen nix vllt nur horizontal mit shear */
-        var rayX = camPlaneOriginX + fx*camRightX - fy*camUpX;
-        var rayY = camPlaneOriginY + fx*camRightY - fy*camUpY;
-        var rayZ = camPlaneOriginZ + fx*camRightZ - fy*camUpZ;
-        var rayNorm = 1.0/Math.sqrt(rayX*rayX + rayY*rayY + rayZ*rayZ);
-
-        var theta = Math.acos(rayY*rayNorm);
-        var phi = Math.atan2(rayZ,rayX);
-        var theta_i = theta_fac*theta;
-        var phi_i = phi_fac*phi;
-
-        var y = theta_i;
-        var x = phi_i + x_shift;
-        line_x1[line_i] = x;
-        line_y1[line_i] = y;
-
-        /*var offset=4*(Math.floor(y / src_height * dest_height) * dest_width * 2 +
-                Math.floor(x / src_width * dest_width) + dest_width);
-
-        pixels[offset]     = 0xff;
-        pixels[offset+1]   = 0xff;
-        pixels[offset+2]   = 0xff;*/
-        line_i++;
-      }
-
-      if(i > 0) {
-        for(var line_i=0; line_i < line_x1.length-1; line_i++) {
-          drawQuad(source.data, src_width, 0,
-                   target.data, dest_width, 0,
-                   line_x2[line_i], line_y2[line_i],
-                   line_x1[line_i], line_y1[line_i],
-                   line_x2[line_i+1], line_y2[line_i+1],
-                   line_x1[line_i+1], line_y1[line_i+1],
-                   line_i * x_res, (i - 1) * y_res, x_res, y_res);
-        }
-      }
-      var t = line_x1;
-      line_x1 = line_x2;
-      line_x2 = t;
-      var t = line_y1;
-      line_y1 = line_y2;
-      line_y2 = t;
-    }
-
-    //upload image data
-    ctx.putImageData(target, 0, 0);
-  }
-}
-
 WebGLRenderer = Class(Object,
     function WebGLRenderer(canvas) {
         this.canvas = canvas;
@@ -407,9 +215,9 @@ WebGLRenderer = Class(Object,
 
             this.gl.useProgram(this.program);
 
+            // Get locations of shader variables.
             this.attribs = {
                 vertex: this.gl.getAttribLocation(this.program, 'vertex'),
-                cam: this.gl.getAttribLocation(this.program, 'cam'),
                 texture: this.gl.getUniformLocation(this.program, 'texture_img'),
                 texture_size: this.gl.getUniformLocation(this.program, 'texture_size'),
                 panorama_height: this.gl.getUniformLocation(this.program, 'panorama_height'),
@@ -419,6 +227,7 @@ WebGLRenderer = Class(Object,
                 cam_plane: this.gl.getUniformLocation(this.program, 'cam_plane'),
             }
 
+            // Prepare texture (even if there is no image data available).
             this.texture = this.gl.createTexture();
             this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
             this.gl.texParameteri(this.gl.TEXTURE_2D,
@@ -426,7 +235,9 @@ WebGLRenderer = Class(Object,
             this.gl.texParameteri(this.gl.TEXTURE_2D,
                     this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
             this.gl.uniform1i(this.attribs.texture, 0);
+            this.gl.activeTexture(this.gl.TEXTURE0);
 
+            // Create buffer for vertex data (only one quad).
             this.vertex_buffer = this.gl.createBuffer();
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertex_buffer);
             this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
@@ -436,14 +247,13 @@ WebGLRenderer = Class(Object,
                     1, -1, 0
                      ]), this.gl.STATIC_DRAW);
 
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertex_buffer);
+            // Enable vertex buffer and set shader variable.
             this.gl.enableVertexAttribArray(this.attribs.vertex);
             this.gl.vertexAttribPointer(this.attribs.vertex, 3, this.gl.FLOAT,
                     false, 3*4, 0*4);
-
-            this.gl.activeTexture(this.gl.TEXTURE0);
         },
-        setImage: function(data, panorama_height) {
+        set_image: function(data, panorama_height) {
+            // Upload image as texture and set dimension variables.
             this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA,
                    this.gl.UNSIGNED_BYTE, data);
             this.gl.uniform1i(this.attribs.texture, 0);
@@ -476,6 +286,11 @@ WebGLRenderer = Class(Object,
 
             return shader;
         },
+        resize: function() {
+            this.gl.uniform2f(this.attribs.viewport_size,
+                    this.canvas.width, this.canvas.height);
+            this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+        },
         draw: function() {
             var ratioUp = Math.tan(cam_fov*DEG2RAD/2.0);
             var ratioRight = ratioUp*(this.canvas.width / this.canvas.height);
@@ -492,15 +307,9 @@ WebGLRenderer = Class(Object,
             var camPlaneOriginY = camDirY + 0.5*camUpY - 0.5*camRightY;
             var camPlaneOriginZ = camDirZ + 0.5*camUpZ - 0.5*camRightZ;
 
-            this.gl.uniform2f(this.attribs.viewport_size,
-                    this.canvas.width, this.canvas.height);
             this.gl.uniform3f(this.attribs.cam_up, camUpX, camUpY, camUpZ);
             this.gl.uniform3f(this.attribs.cam_right, camRightX, camRightY, camRightZ);
             this.gl.uniform3f(this.attribs.cam_plane, camPlaneOriginX, camPlaneOriginY, camPlaneOriginZ);
-
-            this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-            this.gl.clearColor(0.5, 0., 0., 1.);
-            this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
             this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
         }
@@ -521,8 +330,6 @@ function drawRoundedRect(ctx, ox, oy, w, h, radius){
 }
 
 function draw(){
-  renderer.draw();
-  return;
   if(pano_canvas != null && pano_canvas.getContext != null){
     var ctx = pano_canvas.getContext("2d");
 
